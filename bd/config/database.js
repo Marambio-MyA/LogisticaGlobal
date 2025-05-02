f71fba121ca3171f735fbfff7681a9f887ceae2b
zabroso <pmarambi@inf.utfsm.cl>
2025-05-02 18:06:43 -0400

    refactor: bd tiene su propio directorio

--- bd/config/database.js
+++ bd/config/database.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 export const sequelize = new Sequelize(
     process.env.DB_NAME,
