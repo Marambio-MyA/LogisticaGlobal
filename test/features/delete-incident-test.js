cffe0577d9bd6b9d2cf8095446e61fbebc376061
Daniel Villarroel <davillarroelm2000@gmail.com>
2025-06-10 13:49:49 -0400

    boton actualizar y delete test

--- test/features/delete-incident-test.js
+++ test/features/delete-incident-test.js
@@ -31,10 +31,8 @@ async function runDeleteIncidentTest() {
     // Seleccionar última fila
     const rows = await page.$$('tbody.MuiTableBody-root tr');
     const lastRow = rows[rows.length - 1];
-    
-    const actionButtons = await lastRow.$$(':scope td:last-child button');
-    const deleteButton  = actionButtons[0];
-    await deleteButton.click();
+    const deleteIcon = await lastRow.$('[data-testid="DeleteIcon"]');
+    if (!deleteIcon) throw new Error('No se encontró el botón DeleteIcon');
 
     // Escuchar el diálogo de confirmación
     page.once('dialog', async (dialog) => {
